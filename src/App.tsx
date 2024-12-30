578df85ba15b6ea8ca87e814bb0d6c1dbe3d9339
Andrii-77 <andrijsmaga@gmail.com>
2024-12-30 21:22:35 +0200

    Вивчив react 1 2 Компоненти основи.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-      hello okten
+      <MyComponent text={'hello 1'}/>
+      <MyComponent text={'hello 2'}/>
+      <MyComponent text={'hello okten'}/>
+      {/*{MyComponent({text: 'hello 2'})}*/}
     </>
   )
 }
