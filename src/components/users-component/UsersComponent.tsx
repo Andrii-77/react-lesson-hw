3f3b6413da747e48db3e2b6017b0a04c3e8a4b08
Andrii-77 <andrijsmaga@gmail.com>
2025-01-05 11:54:27 +0200

    Вивчив react 2 3 services.

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,15 +1,26 @@
 import {useEffect, useState} from 'react';
 import {IUser} from "../../models/IUser.ts";
 import {UserComponent} from "../user-component/UserComponent.tsx";
+import {getUsers} from "../../services/api.service.ts";
 
 const UsersComponent = () => {
     const [users, setUsers] = useState<IUser[]>([]);
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(response => {
-                setUsers(response);
-            });
+
+        // -----другий варіант-----
+        const fetchData = async () => {
+            const users = await getUsers();
+            setUsers(users);
+
+        }
+        fetchData();
+
+        // -----перший варіант-----
+        // getUsers()
+        //     .then(response => {
+        //         setUsers(response);
+        //     });
+
         return () => {
             console.log('done');
         }
