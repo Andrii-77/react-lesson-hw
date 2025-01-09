2046df2dd30a93156a869bc4a3f068c162ad9ffa
Andrii-77 <andrijsmaga@gmail.com>
2025-01-09 20:03:03 +0200

    Вивчив react 3 3 state forward.

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,6 +3,7 @@ import Layout from "../layouts/Layout.tsx";
 import {HomePage} from "../pages/HomePage.tsx";
 import {UsersPage} from "../pages/UsersPage.tsx";
 import {PostsPage} from "../pages/PostsPage.tsx";
+import SingleUserDetailsPage from "../pages/SingleUserDetailsPage.tsx";
 
 
 
@@ -12,6 +13,7 @@ export const routes = createBrowserRouter([
         path: '', element: <Layout/>, children: [
             {index: true, element: <HomePage/>},
             {path: 'users', element: <UsersPage/>},
+            {path: 'users/details', element: <SingleUserDetailsPage/>},
             {path: 'posts', element: <PostsPage/>},
         ]
     }
