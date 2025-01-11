81cfd3b72aecd76773bb876127f1319064e19dfb
Andrii-77 <andrijsmaga@gmail.com>
2025-01-11 17:36:10 +0200

    Commit finished hw4/task1.

--- src/components/pagination-component/PaginationComponent.tsx
+++ src/components/pagination-component/PaginationComponent.tsx
@@ -1,16 +1,17 @@
 import {useSearchParams} from "react-router";
 
+
 export const PaginationComponent = () => {
     const [searchParams, setSearcParams] = useSearchParams({page: "1"});
     let currentPage = Number(searchParams.get('page') || '1');
     return (
         <div>
-            <button onClick={() => {
+            <button className="border-solid border-2 border-black bg-sky-200" onClick={() => {
                 if(currentPage > 1) {
                 setSearcParams({page: (--currentPage).toString()});
                 }
             }}>prev</button>
-            <button onClick={() => {
+            <button className="border-solid border-2 border-black bg-green-500" onClick={() => {
                 setSearcParams({page: (++currentPage).toString()});
             }}>next</button>
         </div>
