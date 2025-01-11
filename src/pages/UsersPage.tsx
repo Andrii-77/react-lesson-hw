3ced4f5910791515c6115d7bc16a1683bfa8eb41
Andrii-77 <andrijsmaga@gmail.com>
2025-01-11 14:23:55 +0200

    Commit finished hw/4task1.

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,8 +1,11 @@
 import {UsersComponent} from "../components/users-component/UsersComponent.tsx";
+import {PaginationLayout} from "../layouts/PaginationLayout.tsx";
 
 export const UsersPage = () => {
     return (
         <div>
+            <PaginationLayout/>
+            <hr/>
             <UsersComponent/>
         </div>
     );
