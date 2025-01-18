a51a8f56cb8f0dea6079f3809250e3d294dd03e3
Andrii-77 <andrijsmaga@gmail.com>
2025-01-18 16:15:20 +0200

    Вивчив react 7 1 lifecycle memo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
+import {UsersComponent} from "./components/UsersComponent.tsx";
 
 function App() {
 
+  console.log('app');
   return (
-    <>
-
-    </>
-  )
+      <div>
+        <UsersComponent/>
+      </div>
+  );
 }
 
 export default App
