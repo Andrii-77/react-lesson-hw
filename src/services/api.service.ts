70235da8b9b790e46725cb16895a5b3ee26c4d0b
Andrii-77 <andrijsmaga@gmail.com>
2025-01-23 17:42:34 +0200

    Commit hw8/task1-axios for checking by a mentor.

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,4 @@
-import {baseUrl} from "../constants/urls.ts";
+// import {baseUrl} from "../constants/urls.ts";
 
 // export const userService = {
 //     getAllUsers: async (): Promise<IUser[]> => {
@@ -11,9 +11,21 @@ import {baseUrl} from "../constants/urls.ts";
 // const postService = {};
 // const commentService = {};
 
-export const getAll = async <T> (endpoint: string) => {
-        const responseResult = await fetch(`${baseUrl}${endpoint}`).then((response: Response) => response.json());
-        return responseResult as T;
-}
+// export const getAll = async <T> (endpoint: string) => {
+//         const responseResult = await fetch(`${baseUrl}${endpoint}`).then((response: Response) => response.json());
+//         return responseResult as T;
+// }
 
 // const all = getAll<IUser[]>('/users');
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {'Content-Type': 'application/json'}
+});
+
+export const getAll = async <T> (endpoint: string) => {
+    const axiosResponse = await axiosInstance.get<T>(endpoint);
+    return axiosResponse.data;
+}
